c350d575b87a059456b7f7230f53461439f1b720
thjking <thjking@live.com>
2017-08-25 22:21:21 +0800

    加入slope one距离算法

--- users.py
+++ users.py
@@ -34,6 +34,11 @@ users = {
 
 }
 
+users2 = {"Amy":{"Taylor Swift":4,"PSY":3,"Whitney Houston":4},
+		  "Ben":{"Taylor Swift":5,"PSY":2},
+		  "Clara":{"PSY":3.5,"Whitney Houston":4},
+		  "Daisy":{"Taylor Swift":5,"Whitney Houston":3}}
+
 users3 = {"David":{"Imagine Dragons":3,"Daft Punk":5,
 					"Lorde":4,"Fall Out Boy":1},
 		  "Matt":{"Imagine Dragons":3,"Daft Punk":4,
@@ -135,8 +140,8 @@ def computeNearestNeighbor_Pea(username,users):
 	return distances
 # print computeNearestNeighbor_Pea('Hailey',users)
 
-# 利用改进版余弦相似度求物品间相似程度
-def computeSimilarity(band1,band2,userRatings):
+# 利用改进版余弦相似度求物品间相似程度(数值形式)
+def computeSimilarity_num(band1,band2,userRatings):
 	average = {}
 	similarity = {}
 	for (key,rat) in userRatings.items():
@@ -153,10 +158,29 @@ def computeSimilarity(band1,band2,userRatings):
 	if sqrt(num1) * sqrt(num2) == 0:
 		return 0
 	else:
-		#similarity[band1+','+band2] = float(num) / (sqrt(num1) * sqrt(num2))
 		return float(num) / (sqrt(num1) * sqrt(num2))
-		#print band1,',',band2,'----',float(num) / (sqrt(num1) * sqrt(num2))
+# computeSimilarity(content[2],content[4],users3)
 
+# 利用改进版余弦相似度求物品间相似程度(字典形式)
+def computeSimilarity_dic(band1,band2,userRatings):
+	average = {}
+	similarity = {}
+	for (key,rat) in userRatings.items():
+		average[key] = (float(sum(rat.values()))
+		                /len(rat.values()))
+	num = 0
+	num1 = 0
+	num2 = 0
+	for (user,rat) in userRatings.items():
+		if band1 in rat and band2 in rat:
+			num += ((rat[band1] - average[user])*(rat[band2] - average[user]))
+			num1 += pow((rat[band1] - average[user]),2)
+			num2 += pow((rat[band2] - average[user]),2)
+	if sqrt(num1) * sqrt(num2) == 0:
+		return 0
+	else:
+		similarity[band1+','+band2] = float(num) / (sqrt(num1) * sqrt(num2))
+		return similarity
 # computeSimilarity(content[2],content[4],users3)
 
 # 计算任意两物品之间相似度函数（字典形式）
@@ -172,33 +196,83 @@ def computeSimilarity_all(content):
 # print computeSimilarity_all(content)
 
 # 归一化之缩小函数（字典形式）
-def narrow(min,max,users):
+def narrow_dic(min,max,users):
 	for (key,rat) in users.items():
 		for key in rat:
 			rat[key] = float(2 * (rat[key] - min) - (max - min)) / (max - min)
 	return users
-# print narrow(1,5,users3)
+# print narrow_dic(1,5,users3)
+
+# 归一化之缩小函数（数值形式）
+def narrow_num(min,max,num):
+	return float(2 * (num - min) - (max - min)) / (max - min)
+# print narrow)_num(1,5,3)
 
 # 归一化之放大函数（字典形式）
-def enlarge(min,max,users):
+def enlarge_dic(min,max,users):
 	for (key,rat) in users.items():
 		for key in rat:
 			rat[key] = float((rat[key] + 1)) / 2 * (max - min) + min
 	return users
-
-# print enlarge(1,5,narrow(1,5,users3))
+	
+# 归一化之放大函数（数值形式）
+def enlarge_num(min,max,num):
+	return float((num + 1)) / 2 * (max - min) + min
 
 # 基于物品相似度的评分预测函数(字典形式)
-def predict(user,content,users,min,max):
+def predict_dic(user,content,users,min,max):
 	sum1 = 0
 	sum2 = 0
 	dict = {}
-	users = narrow(min,max,users)
+	users = narrow_dic(min,max,users)
 	user_s = users[user]
 	for key in user_s:
 		sum1 += user_s[key] * computeSimilarity(key,content,users)
 		sum2 += abs(computeSimilarity(key,content,users))
 	dict[user+','+content] = float(sum1) / sum2
 	return dict
+	
+# 基于物品相似度的评分预测函数(数值形式)
+def predict_num(user,content,users,min,max):
+	sum1 = 0
+	sum2 = 0
+	dict = {}
+	users = narrow_dic(min,max,users)
+	user_s = users[user]
+	for key in user_s:
+		sum1 += user_s[key] * computeSimilarity(key,content,users)
+		sum2 += abs(computeSimilarity(key,content,users))
+	return float(sum1) / sum2
 
 # 以上数函数待加入数值形式以适应不同计算
+
+def dev(i,j,users):
+	num = 0
+	sum = 0
+	for(key,rat) in users.items():
+		if i in rat and j in rat:
+			num += 1
+	for(key,rat) in users.items():
+		if i in rat and j in rat:
+			sum += float(rat[i] - rat[j]) / num
+	return sum
+print dev('Whitney Houston','PSY',users2)
+
+def computeDeviations(user):
+    frequemcies = {}    #用户评价数字典
+    deviations = {}     #评分偏差累计字典    
+    for ratings in user.values():
+        for(item,rat) in ratings.items():
+            frequemcies.setdefault(item,{})    #加入用户评价数字典的键与初始化键值字典
+            deviations.setdefault(item,{})     #同上
+            for(item2,rat2) in ratings.items():     
+                if item != item2:
+                    frequemcies[item].setdefault(item2,0)    #加入键值字典的键与键值
+                    deviations[item].setdefault(item2,0.0)   #同上
+                    frequemcies[item][item2] += 1            #计算评分人数
+                    deviations[item][item2] += rat - rat2    #累加评分偏差
+    for (item,rat) in deviations.items():
+        for item2 in rat:
+            rat[item2] /= frequemcies[item][item2]    #评分偏差/评分人数
+    return deviations                                 #返回更新后评分偏差字典
+print computeDeviations(users2)
